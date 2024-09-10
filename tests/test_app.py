3fed3a9b30dc81f47ea57decc29c8e36180d567e
willyanfx <willyan.fx@gmail.com>
2024-09-09 21:35:45 -0600

    TEST: POST fixture for the test (DRY)

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,25 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from app import app
-
-
-def test_root_returns_200_and_hello_world():
-    client = TestClient(app)
-
+def test_root_returns_200_and_hello_world(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Hello, World!'}
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'mari',
+            'email': 'mari@example.com',
+            'password': 'veryscret',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'username': 'mari',
+        'email': 'mari@example.com',
+    }
