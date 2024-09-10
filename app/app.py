f9327602775a9bee1cf51fd8ca0795b2eb3d781e
willyanfx <willyan.fx@gmail.com>
2024-09-09 21:35:09 -0600

    create: POST

--- app/app.py
+++ app/app.py
@@ -1,8 +1,23 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from app.schemas import Message, UserDB, UserPublic, UserSchema
 
 app = FastAPI()
 
+database = []
 
-@app.get('/')
+
+@app.get('/', response_model=Message)
 def read_root():
     return {'message': 'Hello, World!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
